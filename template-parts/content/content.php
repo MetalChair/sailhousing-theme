a2324312b6e4a95b81067012e60259435ed3d982
Logan <logan.scott441@gmail.com>
2021-05-03 16:11:20 -0500

    Surpressed homepage header on front page

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -5,11 +5,12 @@
 		if ( is_sticky() && is_home() && ! is_paged() ) {
 			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
 		}
-		if ( is_singular() ) :
+		elseif ( is_singular() ){
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
+		}
+		elseif(! is_front_page()){
 			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		endif;
+		}
 		?>
 	</header><!-- .entry-header -->
 
