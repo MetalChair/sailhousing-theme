bf3fb344f489ab238f7478748ffe1b77ea2a82c3
Logan <logan.scott441@gmail.com>
2021-05-12 22:11:59 -0400

    Added SAIL acronym meaning to header

--- template-parts/header/nav-menu.php
+++ template-parts/header/nav-menu.php
@@ -12,6 +12,7 @@
 <?php if(has_nav_menu('primary')): ?>
     <nav id = "site-navigation" class = "nav-bar">
         <?php get_template_part("template-parts/header/nav", "brand"); ?>
+        <div class = "label-text"><a class = "home-text" href = "/">Solutions for Adult Independent Living</a></div>
         <?php get_template_part("template-parts/header/nav", "items"); ?>
     </nav>
 <?php endif; ?>
