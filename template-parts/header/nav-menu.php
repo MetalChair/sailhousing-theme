2e3291cd1a1f7f57a21b6f077dff1904f481c4ec
Mules <aaronmueller64@gmail.com>
2021-07-13 18:10:24 -0400

    changed a word

--- template-parts/header/nav-menu.php
+++ template-parts/header/nav-menu.php
@@ -13,7 +13,7 @@
     <nav id = "site-navigation" class = "nav-bar">
         <div class = "nav-margin">
             <?php get_template_part("template-parts/header/nav", "brand"); ?>
-            <div class = "label-text"><a class = "home-text" href = "/">Solutions for Adult Independent Living</a></div>
+            <div class = "label-text"><a class = "home-text" href = "/">Solutions for Adult Interdependent Living</a></div>
             <div class="menu-show-container">
                 <i class="fa fa-bars fa-3x menu-show-button" aria-hidden="true"></i>
             </div>
