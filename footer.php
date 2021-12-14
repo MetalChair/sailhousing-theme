da3b3279c998eb3dd02c1bbc173d96bb6f82d7ca
Mules <aaronmueller64@gmail.com>
2021-12-13 22:44:42 -0500

    another try

--- footer.php
+++ footer.php
@@ -1,25 +1,24 @@
+<?php
+?>
 <footer>
-	<div class = "footer-content">
-		<div class = "col">
-			<a href="mailto:info@sailhousingsolutions.org">info@sailhousingsolutions.org</a>
-			<?php
-				dynamic_sidebar( "footer-bar" )
-			;?>
-		</div>
-		<div class = "col">
-			<div>c/o SAIL</div>
-			<div>1669 W Maple Road</div>
-			<div>Birmingham, MI 48009</div>
-		</div>
-		<div class = "col">
-
-			<?php
-				get_template_part( "template-parts/footer/footer", "menu");
-			;?>
-		</div>
-
-	</div>
+<div class = "footer-content">
+<div class = "col">
+<a href="mailto:info@sailhousingsolutions.org">info@sailhousingsolutions.org</a>
+<?php
+dynamic_sidebar( "footer-bar" );
+?>
+</div>
+<div class = "col">
+<div>c/o SAIL</div>
+<div>1669 W Maple Road</div>
+<div>Birmingham, MI 48009</div>
+</div>
+<div class = "col">
+<?php
+get_template_part( "template-parts/footer/footer", "menu");
+?>
+</div>
+</div>
 </footer>
-
 </body>
 </html>
--- header.php
+++ header.php
@@ -14,4 +14,4 @@
 </head>
 <body <?php body_class("grid-container"); ?>>
 <?php wp_body_open(); ?>
-<?php get_template_part( 'template-parts/header/nav' , 'menu'); ?>
+<?php get_template_part('template-parts/header/nav','menu'); ?>
