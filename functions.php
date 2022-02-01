70d88b1d89071bfe4f1b135f1fd89fda18142f5c
Mules <aaronmueller64@gmail.com>
2022-01-31 22:03:18 -0500

    use print_r

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ endif;
 function debug_to_console($data) {
     $output = $data;
     if (is_array($output))
-        $output = implode(',', $output);
+        $output = print_r($output, true);
 
     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
 }
