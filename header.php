79f41e195a2418f925d3294f49ba338dbf9c4e67
Dan <t.dan.muelle@gmail.com>
2022-10-31 18:28:38 -0700

    Clean up header

--- header.php
+++ header.php
@@ -1,14 +1,11 @@
 <?php
 ?>
+<!DOCTYPE html>
 <head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&display=swap" rel="stylesheet">
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="profile" href="https://gmpg.org/xfn/11" />
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&family=Poppins&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
